e87c7ab2977d95d6561e33b0daef7416b2ce2cd2
Moises Velasquez <mvelasquez@pictoric.com>
2022-09-14 23:38:37 -0500

    add todo

--- src/app/todos/todo-add/todo-add.component.ts
+++ src/app/todos/todo-add/todo-add.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/reducer.interface';
+import { createTodo } from '../todo.actions';
 
 @Component({
   selector: 'app-todo-add',
   templateUrl: './todo-add.component.html',
-  styleUrls: ['./todo-add.component.scss']
+  styleUrls: ['./todo-add.component.scss'],
 })
 export class TodoAddComponent implements OnInit {
+  txtInput: FormControl;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private store: Store<AppState>) {
+    this.txtInput = new FormControl('', Validators.required);
   }
 
+  addTodo() {
+    if (this.txtInput.invalid) return;
+
+    this.store.dispatch(createTodo({ text: this.txtInput.value }));
+    this.txtInput.reset();
+  }
+
+  ngOnInit(): void {}
 }
