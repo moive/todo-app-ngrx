2bcc3c05088a781d0c929f5d88e9244c65dc7e58
Moises Velasquez <mvelasquez@pictoric.com>
2022-09-16 08:24:30 -0500

    working todo list

--- src/app/todos/todo.reducer.ts
+++ src/app/todos/todo.reducer.ts
@@ -2,7 +2,10 @@ import { createReducer, on } from '@ngrx/store';
 import { Todo } from './models/todo';
 import { createTodo } from './todo.actions';
 
-export const initialState: Todo[] = [new Todo('Saving to world')];
+export const initialState: Todo[] = [
+  new Todo('Saving to world'),
+  new Todo('testing'),
+];
 
 export const todoReducer = createReducer(
   initialState,
