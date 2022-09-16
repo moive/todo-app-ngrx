a2f425afacdd8cc09c3f85131d1ad4aabbb50ffb
Moises Velasquez <mvelasquez@pictoric.com>
2022-09-16 09:29:32 -0500

    controls items

--- src/app/todos/todo-item/todo-item.component.ts
+++ src/app/todos/todo-item/todo-item.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Todo } from '../models/todo';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.scss']
+  styleUrls: ['./todo-item.component.scss'],
 })
 export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo = new Todo('');
+  @ViewChild('editRef') txtInputEdit!: ElementRef;
+  chkCompleted!: FormControl;
+  txtInput!: FormControl;
 
-  constructor() { }
+  editing: boolean = false;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.chkCompleted = new FormControl(this.todo.completed);
+    this.txtInput = new FormControl(this.todo.text, Validators.required);
   }
 
+  edit() {
+    this.editing = true;
+    setTimeout(() => {
+      this.txtInputEdit.nativeElement.select();
+    }, 1);
+  }
+
+  finishedEditing() {
+    this.editing = false;
+  }
 }
